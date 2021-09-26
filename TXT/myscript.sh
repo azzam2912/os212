f04b1f6aa560cf5aa5f6038703da066fc16efc25
azzam2912 <63704395+azzam2912@users.noreply.github.com@users.noreply.github.com>
2021-09-26 22:35:25 +0700

    fix error 1

--- TXT/myscript.sh
+++ TXT/myscript.sh
@@ -13,7 +13,7 @@
 # You new to set "REC2" with your own Public-Key Identity!
 # Check it out with "gpg --list-key"
 
-REC2="azzam2912@localhost"
+REC2="azzam.labib@ui.ac.id"
 REC1="operatingsystem@vlsm.org"
 FILES="my*.asc my*.txt my*.sh"
 SHA="SHA256SUM"
